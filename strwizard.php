6c45d0f72e5dac61d8d60fc0fd739b2f23fe9ea9
omurserdarr <omurserdarr@gmail.com>
2021-04-08 00:40:48 +0300

    removed $arg from charcipher function

--- strwizard.php
+++ strwizard.php
@@ -26,7 +26,19 @@ class StrWizard
 
     //callable
 
+    /*
     public function charCipher(string $arg,int $limit=self::LIMIT):string
+    {
+        $secret="";
+        for($i=0;$i<$limit;$i++){
+            $randChar=array_rand($this->chars,1);
+            $secret.=$this->chars[$randChar];
+        }
+        return $secret;
+    }
+    */
+
+    public function charCipher(int $limit=self::LIMIT):string
     {
         $secret="";
         for($i=0;$i<$limit;$i++){
@@ -95,5 +107,6 @@ $text="say hello to my little function";
 //print_r($wiz->delSpesificChars(array("0","A","a","X","x")));
 //print_r($wiz->delSpesificAllChars(array("0","A","a","X","x","[","]","#","!")));
 //echo $wiz->primitiveCipher("say hello to my little function");
-echo $wiz->charCipher($text);
-echo $wiz->charCipher($text,64);
+//echo $wiz->charCipher($text);
+//echo $wiz->charCipher($text,64);
+echo $wiz->charCipher(64);
